221d953d82b32c3d2a7509c57558834f3664d877
boardguy1024 <boardguy1024@gmail.com>
2021-06-04 17:28:36 +0900

    Initial Commit

---
+++ SearchingAppSwiftUI/SearchingAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchingAppSwiftUIApp.swift
+//  SearchingAppSwiftUI
+//
+//  Created by park kyung seok on 2021/06/04.
+//
+
+import SwiftUI
+
+@main
+struct SearchingAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
